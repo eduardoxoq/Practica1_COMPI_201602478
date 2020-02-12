3b9082e9dd48884a7c72346646caca1b471c207b
Eduardo Xoquic <eduardoxoq@gmail.com>
2020-02-12 16:29:57 -0600

    Primer Commit

---
+++ src/practica1_201602478/Practica1_201602478.java
@@ -0,0 +1,18 @@
+
+package practica1_201602478;
+
+/**
+ *
+ * @author Eduardo Xoquic
+ */
+public class Practica1_201602478 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
+    }
+    
+}
